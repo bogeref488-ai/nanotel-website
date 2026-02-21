bb810233881e092a78921d32da8029195577b9bf
bogeref488-ai <bogeref488@gmail.com>
2026-02-21 21:17:50 +0300

    Fix routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-// Google Analytics page tracking for React routes
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-
-function AnalyticsTracker() {
-  const location = useLocation();
-
-  useEffect(() => {
-    window.gtag('config', 'G-RSMM1JVGWS', {
-      page_path: location.pathname + location.hash,
-    });
-  }, [location]);
-
-  return null;
-}
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AnalyticsTracker />
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
